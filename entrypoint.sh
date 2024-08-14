a85507fa50fe906871bcd8a34851eacfd92062bb
ciaqui <ciara@u1trav101.net>
2024-08-14 16:52:11 +0200

    configs can have .yaml extension as well .yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,10 +18,14 @@ if test -e "$SOURCE_DIRECTORY/Gemfile" && ! bundle check --dry-run --gemfile "$S
 fi
 
 echo "Configuring Jekyll..."
-if test -f "${SOURCE_DIRECTORY}/_config.yml"; then
-  python3 /merge_configs.py "${SOURCE_DIRECTORY}/_config.yml"
+if test -f "$SOURCE_DIRECTORY/_config.yml"; then
+  python3 /merge_configs.py "$SOURCE_DIRECTORY/_config.yml"
+elif test -f "${SOURCE_DIRECTORY}/_config.yaml"; then
+  python3 /merge_configs.py "$SOURCE_DIRECTORY/_config.yaml"
 elif test -f "${GITHUB_WORKSPACE}/_config.yml"; then
   python3 /merge_configs.py "${GITHUB_WORKSPACE}/_config.yml"
+elif test -f "${GITHUB_WORKSPACE}/_config.yaml"; then
+  python3 /merge_configs.py "${GITHUB_WORKSPACE}/_config.yaml"
 else
   python3 /merge_configs.py
 fi
